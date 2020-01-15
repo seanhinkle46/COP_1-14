698a02988df7aa361a94a570729f650e04b37399
Sean Hinkle <seanhinkle@Seans-MacBook-Pro.local>
2020-01-14 19:09:30 -0500

    Really long comment

---
+++ src/SecondProgram.java
@@ -0,0 +1,12 @@
+public class SecondProgram {
+	public static void main(String[] args) {
+		int age = 12;
+		double weight = 123.56789;
+		System.out.printf("Hello everyone!%nI am %d years old%n", age);
+		System.out.printf("I weigh %.2f pounds%n", weight);
+		System.out.println("Hello again everyone!%n");
+		
+		System.out.println(10/0.000001);
+		System.out.println(10%12);
+	}
+}
